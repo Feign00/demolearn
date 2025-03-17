a61e32e01e0c76411a0215c72f041e43bdba098e
feign <22255744553@qq.com>
2025-03-17 15:09:02 +0800

    :feat 添加测试 Servlet 类并配置版本控制
    - 新增 TestServlet01 类，处理 doGet 和 doPost 请求
    - 添加 .idea/vcs.xml 文件，配置项目版本控制为 Git

---
+++ src/main/java/ch4/TestServlet01.java
@@ -0,0 +1,23 @@
+package ch4;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/test01")
+public class TestServlet01 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        resp.getWriter().println("1145141919810");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+        resp.getWriter().println("1145141919810");
+    }
+}
